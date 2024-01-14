3bd3d9fa91ba833829d5811addb58f7c18e5987b
geonhopark <rjsghslasp@gmail.com>
2024-01-14 22:05:29 +0900

    some init msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+   
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/home';
+import SubPage from './components/subpage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          reactInstall 테스트
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>{
+  return(
+    <div>
+      <Router>
+        <ul className='tab'>
+          <Link to='/home'><li>home</li></Link>
+          <Link to='/subpage'><li>SubPage</li></Link>
+        </ul>
+        <div className='contents'>
+          <Routes>
+            <Route path='/home' element={<Home/>} />
+            <Route path='/subpage' element={<SubPage/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
