c185f2905a7852cab7faa7dd554bc2dd3a12221c
박건호 <rjsghslasp@gmail.com>
2024-01-24 20:06:20 +0900

    [링크연결] - url과 다른 페이지 노출 및 내부페이지 경로 url에 노출하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link, useParams} from 'react-router-dom';
+
+import Header from './pages/Header';
+import About from './pages/About';
+import Detail from './pages/Detail';
+import ResetPasswd from './pages/mypages/ResetPasswd';
+
+
 
 function App() {
   return (
     <div className="App">
-22
+      <BrowserRouter> 
+        <Header />
+
+        <Routes>
+          <Route path='/About' element={<About/>}></Route>
+
+          <Route path='/Home' element={<About/>}></Route>
+          <Route path='/Detail' element={<Detail/>}></Route>
+          <Route path='/pages/mypages/ResetPasswd' element={<ResetPasswd/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
