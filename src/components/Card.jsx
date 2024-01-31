360be6d706dd43f660a9433c7101a2ccbae8dd09
jeong-hoi-seok <jsk2343@naver.com>
2024-01-31 21:34:32 +0900

    [연습] - 넥스트에서 폴더 구조로 라우터 연결하기

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import styled from 'styled-components';
 
 const Card = () => {
     return (
         <Area>
-            
+            222
+            <div>22</div>
         </Area>
     );
 };
 
-const Area = styled.div`
-    color: red
-    height: 100px
+let Area = styled.button`
+    color: red;
+    height: auto;
 `
-
+Area = styled.button`
+color : blue;
+`
 export default Card;
