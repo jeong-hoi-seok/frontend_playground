d86d4896561a62a47279cf8072c363fb1d1916eb
박건호 <rjsghslasp@gmail.com>
2024-02-07 15:31:32 +0900

    [수정] - 타입스크립트 헤더 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useRouter } from "next/router";
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
 
-class Header extends Component {
-
-    render() {
-        console.log('11')
-        return (
-            <HeaderList>
-                <List>1221</List>
-                <List>1221</List>
-                <List>1221</List>
-                <List>1221</List>
-            </HeaderList>
-        );
+const navList = [
+    {
+        pageName : '비디오 리스트',
+        link : '/videoList'
     }
-}
+]
+
+const Header = (props) => {
+    const {
+        children
+    } = props;
+
+    const router = useRouter();
+    return (
+        <>
+         <HeaderList>
+            {
+                navList.map((item, idx)=>{
+                    return(
+                        <Link key={idx} onClick={()=>{router.push(`${item.link}`)}}>{item.pageName} →</Link>
+                    )
+                })
+            }
+        </HeaderList>
+         {children}
+        </>
+    );
+};
 
 const HeaderList = styled.ul`
-    
+    width: 100%;
+    display: flex;
+    justify-content: center;
 `
-const List = styled.li`
-    
+const Link = styled.li`
+    list-style: none;
+    width: 300px;
+    height: 50px;
+    border: 1px solid #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
 `
 
 export default Header;
--- src/pages/videoList/index.tsx
+++ src/pages/videoList/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useLayoutEffect, useRef, useCallback, useMemo } from "react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const videoList = () => {
+    const router = useRouter();
     return (
-        <div>
-            sdsasdsadasdsad
-        </div>
-    );
+        <>
+            <button onClick={()=>{router.back()}}>뒤로가기</button>
+        </>
+    )
 };
 
 export default videoList;
