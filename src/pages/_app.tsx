7307cb781ef7d480062cb439640d6336813053c6
jeong-hoi-seok <jsk2343@naver.com>
2024-02-07 15:31:32 +0900

    [수정] - 타입스크립트 헤더 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,9 @@ import type { AppProps } from "next/app";
 import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(<Component {...pageProps} />)
+  return(
+    <Header>
+      <Component {...pageProps} />;
+    </Header>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <VideoList />
       </main>
     </>
   );
 }
 
-
-const Test = styled.p`
-  color: red;
-`
